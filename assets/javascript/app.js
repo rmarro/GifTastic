04622adde12820e7056614693e33eef4cba80b68
rmarro <rmarro47@gmail.com>
2017-12-27 18:29:03 -0600

    made initial files and started pseudocoding

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// array of words
+// loop through and make buttons of each with word as a data-something
+// on button click
+    // make the data- the search of the api querey
+    // make all a bunch of images with the json returned
+        // for each image it should have an animated and a still verison
+            // and a data-status
+        // on click of image change the status to animated or still
+        
