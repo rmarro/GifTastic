b39ca0af5fd37f9d75bb650453a8c36865ffa99f
rmarro <rmarro47@gmail.com>
2018-01-02 23:34:27 -0600

    create images, add still and animated urls for each image, made to do list

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,35 +11,49 @@
 
 var topics = ["Harry Potter", "Dumbledore", "Hermione", "Ron"];
 
+// Make a button for each item in topics array
 function makeButtons() {
     for (var i = 0; i < topics.length; i++) {
         var newButton = $("<button>");
         newButton.text(topics[i]);
+        // Save the item as the data-word attribute
         newButton.attr("data-word", topics[i]);
-        $(".buttons").prepend(newButton);
+        $(".buttons").append(newButton);
     }
 };
 
-// function makeGifs(){
-//     for (var i = 0; i < 10; i++)
-//        response.data[i]
-// }
-
-
-
 makeButtons();
 
+// When button is clicked
 $("button").on("click", function() {
+    $(".gifs").empty();
+    // Make queryURL with data-word attribute as search term
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Vo3yan4o4RtxSVlfIia2Zlsk8C7I5FP3&limit=10&q="
     + $(this).attr("data-word")
 
+    // Make and ajax call via GET to the url
     $.ajax({
         url: queryURL,
         method: "GET"
     })
 
+    // When ajax call complete create callback with parameter called response
     .done(function(response) {
-        // makeGifs()
+        // For all 10 objects in the JSON
+        for (var i = 0; i < 10; i++) {
+            // Create a new image that has still and animated urls as attributes
+            var newGif = $("<img>");
+            newGif.attr("data-still", response.data[i].images.fixed_width_still.url);
+            newGif.attr("data-animate", response.data[i].images.fixed_width.url);
+            newGif.attr("src", response.data[i].images.fixed_width_still.url);
+            $(".gifs").prepend(newGif)
+        }
     })
-
 })
+
+
+// TO DO
+// add data-state
+// add onclick image to change src based on state
+// add rating div to display rating
+// add form (make sure button click push only applies to form button)
